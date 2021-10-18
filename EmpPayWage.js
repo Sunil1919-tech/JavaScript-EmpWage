530fd1b26e1abee0d5ae7e7389ec0110e6f64710
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-18 14:26:43 +0530

    Added:Employee attendance

--- EmpPayWage.js
+++ EmpPayWage.js
@@ -1,1 +1,10 @@
 console.log("Welcome to EmpWage computation using JavaScript");
+
+//UC1-Checking Employee Attendence
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee Is ABSENT");
+} else {
+    console.log("Employee Is PRESENT");
+}
