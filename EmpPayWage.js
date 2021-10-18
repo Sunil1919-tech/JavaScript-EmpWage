4673e9d869848db097eaa2ad1fa2967caf5bd98a
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-18 14:44:26 +0530

    Refactored: Using Function to calculate empWage

--- EmpPayWage.js
+++ EmpPayWage.js
@@ -9,24 +9,27 @@ if (empCheck == IS_ABSENT) {
     console.log("Employee Is PRESENT");
 }
 
-//UC2-daily employeeWage
+//UC3--Refactored the code using Function
 
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 {
-    let empHrs=0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch(empCheck){
-        case IS_PART_TIME:
-            empHrs= PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            empHrs= FULL_TIME_HOURS;
-            break;
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
     }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage is:"+ empWage);
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee Wage is:" + empWage);
 }
