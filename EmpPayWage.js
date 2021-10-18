1c5d865fb04c46b97de72a19c80ad37af505566f
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-18 15:04:38 +0530

    Added:Wage for 20 working days

--- EmpPayWage.js
+++ EmpPayWage.js
@@ -1,21 +1,12 @@
 console.log("Welcome to EmpWage computation using JavaScript");
 
-//UC1-Checking Employee Attendence
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee Is ABSENT");
-} else {
-    console.log("Employee Is PRESENT");
-}
-
-//UC3--Refactored the code using Function
-
+//UC4-Computing The Employee Wage By Usimg For Loop
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const WORKING_DAYS = 20;
 {
     function getWorkingHours(empCheck) {
         switch (empCheck) {
@@ -28,8 +19,10 @@ const WAGE_PER_HOUR = 20;
         }
     }
     let empHrs = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = getWorkingHours(empCheck);
+    for (let day = 0; day < WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHrs += getWorkingHours(empCheck);
+    }
     let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Employee Wage is:" + empWage);
+    console.log("Hours " + empHrs + " Emp Wage " + empWage);
 }
